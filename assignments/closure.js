58d424b04676e3b2b5167c81eb435dec67379874
hsrsbd <hsraffety@gmail.com>
2019-05-07 15:19:37 -0700

    mvp~

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,9 +1,27 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+hideAndSeek();
+function hideAndSeek() {
+  const num1 = 43;
+  console.log(`${num1} is a perfectly fine number.`);
+
+  canYouSeeMe();
+  function canYouSeeMe() {
+    const num2 = 93;
+    console.log(`${num2} is also fine.`);
+
+    // youCantSeeMe();
+    function youCantSeeMe() {
+      const num3 = 949 * 13;
+      console.log(
+        `${num3} shall never see the light of day unless it's invoked.`
+      );
+    }
+  }
+}
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
